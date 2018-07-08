ab7117d0bd1edb0bb79352260894a5512db3b7b0
Christiana Vandermale <christianavdm@gmail.com>
2018-07-08 00:48:42 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,9 +15,13 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      @@cart.each do |item|
-        resp.write "#{item}\n"
-      end
+      if @@cart.empty? 
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end    
     elsif req.path.match(/add/)
       item = req.params["q"]
       handle_search(item)
